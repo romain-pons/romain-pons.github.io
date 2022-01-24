9e5284281c128aff4d93f486b84a9a229c91849f
romain-pons <75258269+romain-pons@users.noreply.github.com>
2022-01-24 20:35:41 +0100

    Delete Jo2024 directory

--- Jo2024/index.js
+++
@@ -1,6 +0,0 @@
-let sidenav = document.querySelector(".sidenav");
-let trigger = document.querySelector(".sidenav-trigger");
-
-trigger.addEventListener("click", () => {
-  sidenav.classList.contains("active") ? sidenav.classList.remove("active") : sidenav.classList.add("active");
-});
